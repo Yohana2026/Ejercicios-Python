11b91bed267b9d310fcf709513569faada1efd65
Yohana Triana <ytaranzola@gmail.com>
2026-04-16 21:57:10 +0200

    t1e1

--- ejercicios.py/t1_e1.py
+++ ejercicios.py/t1_e1.py
@@ -1,1 +1,13 @@
-nombre_pelicula
+
+print ("Informacion sobre tu pelicula favorita")
+
+pelicula_favorita = input("Título de la película")
+
+director_pelicula = input("Director de la película")
+
+anno_lanzamiento = input("Año de lanzamiento")
+
+genero = input("Género")
+
+duracion = input("Duración - minutos")
+
