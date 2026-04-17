84bc82598822049b30bc70cfde61eb649c4e46ad
Yohana Triana <ytaranzola@gmail.com>
2026-04-17 10:11:35 +0200

    Update t1_e1

--- ejercicios.py/t1_e1.py
+++ ejercicios.py/t1_e1.py
@@ -1,13 +1,14 @@
 
 print ("Informacion sobre tu pelicula favorita")
 
-pelicula_favorita = input("Título de la película")
+pelicula_favorita = input("Título de la película: ")
 
-director_pelicula = input("Director de la película")
+director_pelicula = input("Director de la película: ")
 
-anno_lanzamiento = input("Año de lanzamiento")
+anno_lanzamiento = input("Año de lanzamiento: ")
 
-genero = input("Género")
+genero = input("Género: ")
 
-duracion = input("Duración - minutos")
+duracion = input("Duración - minutos: ")
 
+premios = input("¿Tiene premios? (verdadero o falso): ")
