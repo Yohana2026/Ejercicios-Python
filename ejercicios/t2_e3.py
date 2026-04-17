5420f67afe83e88429e20ab6e4c5d355ffee371b
Yohana Triana <ytaranzola@gmail.com>
2026-04-17 11:42:29 +0200

    Terminado t2_e3

---
+++ ejercicios/t2_e3.py
@@ -0,0 +1,19 @@
+print("Frases")
+
+intro_frase = input("Introduzca una frase:")
+
+longitud_frase = len(intro_frase)
+
+print("La longitud de la frase en caracteres es: ", longitud_frase)
+
+frase_mayu = intro_frase.upper()
+
+print("Frase en mayúsculas: ",frase_mayu)
+
+frase_minus = intro_frase.lower()
+
+print("Frase en minúsculas: ",frase_minus)
+
+
+
+
