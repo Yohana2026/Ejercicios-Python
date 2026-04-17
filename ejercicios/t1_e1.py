a5009811f5960ec917a81d2a1e57e25baf3c735d
Yohana Triana <ytaranzola@gmail.com>
2026-04-17 10:27:41 +0200

    Add valor variable ingles

---
+++ ejercicios/t1_e1.py
@@ -0,0 +1,28 @@
+
+print ("Informacion sobre tu pelicula favorita")
+
+pelicula_favorita = input("Título de la película: ")
+
+director_pelicula = input("Director de la película: ")
+
+anno_lanzamiento = input("Año de lanzamiento: ")
+
+genero = input("Género: ")
+
+duracion = input("Duración - minutos: ")
+
+premios = input("¿Tiene premios? (verdadero o falso): ")
+
+print ("Information about your favorite movie")
+
+pelicula_favorita = input("Movie title: ")
+
+director_pelicula = input("Film Director: ")
+
+anno_lanzamiento = input("Year of release: ")
+
+genero = input("Genre: ")
+
+duracion = input("Duration - minutes: ")
+
+premios = input("Does it have any awards? (true or false): ")
