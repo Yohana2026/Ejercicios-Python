beda34d5b6e17ecaa3019a782e1f685e9735493c
Yohana Triana <ytaranzola@gmail.com>
2026-04-17 15:33:40 +0200

    Create variables t3_e1

---
+++ ejercicios/t3_e1.py
@@ -0,0 +1,14 @@
+print("Precio total de un producto")
+
+nombre_producto = input("Nombre del producto: ")
+
+precio_unidad = float(input("Precio por unidad: "))
+
+cantidad_comprar = float(input("Cantidad a comprar: "))
+
+descuento_porcentaje = float(input("Descuento en porcentaje: "))
+
+iva_porcentaje = float(input("IVA en porcentaje: "))
+
+def precio_total():
+    
