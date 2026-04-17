690b77dc771846813893f4dd8f02c0bb72fc92be
Yohana Triana <ytaranzola@gmail.com>
2026-04-17 11:03:18 +0200

    terminado t2_e1

---
+++ ejercicios/t2_e1.py
@@ -0,0 +1,24 @@
+print ("Calculadora")
+
+num_1 = float(input("Introduzca un número: "))
+
+num_2 = float(input("Introduzca otro numero: "))
+
+suma = num_1 + num_2
+
+resta = num_1 - num_2
+
+multiplicacion = num_1 * num_2
+
+division = num_1 / num_2
+
+print ("Suma", suma)
+
+print ("Resta", resta)
+
+print ("Multiplicación", multiplicacion)
+
+print ("División", division)
+
+
+ 
