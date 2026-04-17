3c0f44733ee8c1654c2a0d6042268500f5819391
Yohana Triana <ytaranzola@gmail.com>
2026-04-17 11:46:33 +0200

    Terminado t2_e4

---
+++ ejercicios/t2_e4.py
@@ -0,0 +1,15 @@
+print("Correo electrónico")
+
+intro_email = input("Introduzca dirección de email:")
+
+longitud_email = len(intro_email)
+
+print("La longitud del email en caracteres es: ", longitud_email)
+
+email_mayu = intro_email.upper()
+
+print("Email en mayúsculas: ",email_mayu)
+
+email_minus = intro_email.lower()
+
+print("Frase en minúsculas: ",email_minus)
