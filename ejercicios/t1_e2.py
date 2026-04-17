7f074ee8dc1812c31acf6dc44d74816063e2d998
Yohana Triana <ytaranzola@gmail.com>
2026-04-17 10:45:14 +0200

    terminado t1_e2

---
+++ ejercicios/t1_e2.py
@@ -0,0 +1,28 @@
+print ("Canción favorita")
+
+titulo_cancion_fav = input("Título de la canción: ")
+
+artista = input("Artista: ")
+
+album = input("Álbum: ")
+
+anno_lanzamiento = input("Año de lanzamiento: ")
+
+duracion_segundos = input("Duración - segundos: ")
+
+videoclip = input("¿Tiene videoclip? (Verdadero/Falso): ")
+
+
+print ("Canción que menos te gusta")
+
+titulo_cancion_no_fav = input("Título de la canción: ")
+
+artista = input("Artista: ")
+
+album = input("Álbum: ")
+
+anno_lanzamiento = input("Año de lanzamiento: ")
+
+duracion_segundos = input("Duración - segundos: ")
+
+videoclip = input("¿Tiene videoclip? (Verdadero/Falso): ")
