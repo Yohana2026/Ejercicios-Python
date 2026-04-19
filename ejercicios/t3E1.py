f0397a54220607e256fb9dc5462d9d0f9fcc4efa
Yohana Triana <ytaranzola@gmail.com>
2026-04-19 19:28:14 +0200

    Update t3E1

--- ejercicios/t3E1.py
+++ ejercicios/t3E1.py
@@ -4,9 +4,44 @@ nombreProducto = input("Nombre del producto: ")
 
 precioUnidad = float(input("Precio por unidad: "))
 
-cantidadComprar = float(input("Cantidad a comprar: "))
+cantComprar = float(input("Cantidad a comprar: "))
 
-descuentoPorcentaje = float(input("Descuento en porcentaje: "))
+descuentoPorcentaje = float(input("Descuento (porcentaje): "))
 
 ivaPorcentaje = float(input("IVA en porcentaje: "))
+
+#precio total antes de descuento
+def precioTotal():
+    precioPagar = precioUnidad * cantComprar
+    return precioPagar
+
+print("El precio total es " + str(precioTotal()))
+
+#descuento del porcentaje
+def descPorc ():
+    precioDesc = (descuentoPorcentaje / 100) * precioTotal()
+    return precioDesc
+
+print("El descuento es " + str(descPorc()))
+
+#precio total con descuento
+def totalYDesc():
+    total = precioTotal() - descPorc() 
+    return total 
+
+
+print("El total aplicando descuento es " + str(totalYDesc()))
+ 
+def calculoIva():
+    iva = ivaPorcentaje / 100
+    return iva
+
+def totalDescIva():
+    total = (totalYDesc() * calculoIva()) + totalYDesc()
+    return total
+
+print ("Usted debe pagar: " + str(totalDescIva()))
+
+
+
 
