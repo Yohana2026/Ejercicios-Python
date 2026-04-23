1431da06220bcbd379efd84652d31c6de817932a
Yohana Triana <ytaranzola@gmail.com>
2026-04-23 23:26:18 +0200

    Summary

--- ejercicios/t4E2.py
+++ ejercicios/t4E2.py
@@ -2,9 +2,13 @@ print("Adivina el número")
 introNum = int(input("Por favor, introduzca un número del 1 al 10: "))
 
 def numGanador(numero):
-    if numero == 4:
-        print("Haz acertado al número ganador")
+    if numero < 1 or numero > 10:
+     print("El número introducido no se encuentra entre el 1 y el 10 ")
     else:
+     if numero == 4:
+        print("Haz acertado al número ganador")
+     else:
         print("El número introducido no ha resultado ser el ganador")
 
 numGanador(introNum)
+
---
+++ ejercicios/t5E1.py
@@ -0,0 +1,16 @@
+print("Calculadora de nota media")
+cantNotas = int(input("¿Cuántas notas desea introducir?: "))
+
+
+def introNota(nota):
+    for i in range(cantNotas):
+    float(input("Introduzca la nota: "))
+    miLista =[introNota(nota), introNota(nota) +1]
+    print(str.miLista)
+
+introNota(cantNotas)  
+
+
+
+        
+
