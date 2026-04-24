b4494cd08a4ed146dea9dd04a21f51a304353c59
Yohana Triana <ytaranzola@gmail.com>
2026-04-24 21:32:58 +0200

    Create t5E2

---
+++ ejercicios/t5E2.py
@@ -0,0 +1,8 @@
+print("Premio colores")
+
+for i in range(5):
+   valor = input("Ingrese un color: ")
+   if valor != "azul":
+      print("Pruebe otro color. ")
+   else:
+      print("Ha obtenido el premio")
